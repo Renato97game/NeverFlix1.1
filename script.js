b9f3d290e26eab406db5b3c6524e7368b13e33dd
Renato97game <127274081+Renato97game@users.noreply.github.com>
2025-06-28 22:33:42 -0300

    ver 28-06-25

---
+++ script.js
@@ -0,0 +1,37 @@
+// Esconde o splash screen depois de 2 segundos
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    document.getElementById('splash-screen').style.display = 'none';
+  }, 2000);
+});
+
+
+const slides = document.querySelectorAll('.banner-slide');
+const dots = document.querySelectorAll('.dot');
+let currentIndex = 0;
+
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove('active'));
+  dots.forEach(dot => dot.classList.remove('active'));
+
+  slides[index].classList.add('active');
+  dots[index].classList.add('active');
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+}
+
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    currentIndex = index;
+    showSlide(currentIndex);
+  });
+});
+
+setInterval(nextSlide, 5000);
+
+
+
+
