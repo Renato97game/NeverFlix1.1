60f659272d300bb875ab85f717c09f8b13bd5e79
Renato Silva <renato97stockgame@gmail.com>
2025-06-29 01:54:25 -0300

    alteração js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 // Esconde o splash screen depois de 2 segundos
 window.addEventListener('load', () => {
   setTimeout(() => {
     document.getElementById('splash-screen').style.display = 'none';
   }, 2000);
 });
-
 
 const slides = document.querySelectorAll('.banner-slide');
 const dots = document.querySelectorAll('.dot');
@@ -32,46 +30,3 @@ dots.forEach((dot, index) => {
 });
 
 setInterval(nextSlide, 5000);
-
-
-
-
-=======
-// Esconde o splash screen depois de 2 segundos
-window.addEventListener('load', () => {
-  setTimeout(() => {
-    document.getElementById('splash-screen').style.display = 'none';
-  }, 2000);
-});
-
-
-const slides = document.querySelectorAll('.banner-slide');
-const dots = document.querySelectorAll('.dot');
-let currentIndex = 0;
-
-function showSlide(index) {
-  slides.forEach(slide => slide.classList.remove('active'));
-  dots.forEach(dot => dot.classList.remove('active'));
-
-  slides[index].classList.add('active');
-  dots[index].classList.add('active');
-}
-
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % slides.length;
-  showSlide(currentIndex);
-}
-
-dots.forEach((dot, index) => {
-  dot.addEventListener('click', () => {
-    currentIndex = index;
-    showSlide(currentIndex);
-  });
-});
-
-setInterval(nextSlide, 5000);
-
-
-
-
->>>>>>> b9f3d290e26eab406db5b3c6524e7368b13e33dd
